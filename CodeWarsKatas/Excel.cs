4b62afd8bb6b7a9675479e84527de6796d320459
Aleksey <aleksey.dantist@gmail.com>
2022-08-10 13:25:12 +0600

    feat: delegate start

---
+++ CodeWarsKatas/Excel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWarsKatas
+{
+    internal class Excel
+    {
+        public Excel()
+        {
+            
+        }
+    }
+}
--- CodeWarsKatas/Program.cs
+++ CodeWarsKatas/Program.cs
@@ -5,6 +5,43 @@ using System.Text.RegularExpressions;
 using System.Linq;
 using System.Xml.Linq;
 
+delegate void StringProcessor(string input);
+
+class Person
+{
+    string name;
+    public Person(string name)
+    {
+        this.name = name;
+    }
+    public void Say(string message)
+    {
+        Console.WriteLine("{0} says: {1}", name, message);
+    }
+}
+class Background
+{
+    public static void Note(string note)
+    {
+        Console.WriteLine("({0})", note);
+    }
+}
+
+class SimpleDelegateUse
+{
+    static void Main()
+    {
+        Person jon = new Person("Jon");
+        Person tom = new Person("Tom");
+        StringProcessor jonsVoice, tomsVoice, background;
+        jonsVoice = new StringProcessor(jon.Say);
+        tomsVoice = new StringProcessor(tom.Say);
+        background = new StringProcessor(Background.Note);
+        jonsVoice("Hello, son.");
+        tomsVoice.Invoke("Hello, Daddy!");
+        background("An airplane files past.");
+    }
+}
 class LearnSharp
 {
     public LearnSharp()
